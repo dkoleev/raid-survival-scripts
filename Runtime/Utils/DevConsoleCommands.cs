fa126a87db833f4df5501cbf667bb2ef657e7e4e
dmitry.koleev <dmitry.koleev@game-garden.com>
2024-10-01 10:27:07 +0300

    add dev console

---
+++ Runtime/Utils/DevConsoleCommands.cs
@@ -0,0 +1,13 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using Yogi.UnityDevConsole.Scripts.Runtime;
+
+namespace Yogi.RaidSurvival.Runtime.Utils {
+    [UsedImplicitly]
+    public static class DevConsoleCommands {
+        [DevConsoleCommand("echo")]
+        public static void Echo(string text) {
+            Debug.Log(text);
+        }
+    }
+}
